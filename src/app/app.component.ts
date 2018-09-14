12ef5a43e7eda0dbdaa8f0968259d6ff22470b62
Fernando <fernando.galarcio@gmail.com>
2018-09-14 01:28:41 -0500

    config conexión firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularFireCourse';
+
+  constructor(private af: AngularFireModule ) {
+    this.af.database.object(`connected`)
+      .subscribe(console.log);
+
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireBaseConfig = {
+    apiKey: 'AIzaSyBe-g3ooa9vq3_ur3R7v0gtT2cnm74evs4',
+    authDomain: 'bulidingappswithangularf-35ef9.firebaseapp.com',
+    databaseURL: 'https://bulidingappswithangularf-35ef9.firebaseio.com',
+    projectId: 'bulidingappswithangularf-35ef9',
+    storageBucket: 'bulidingappswithangularf-35ef9.appspot.com',
+    messagingSenderId: '40429766882'
+  }
 };
 
 /*
